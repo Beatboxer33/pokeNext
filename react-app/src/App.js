7c0be883042569503ef7859acb23aa623922af36
Beatboxer33 <jp_saban@yahoo.fr>
2025-02-20 15:23:05 +0100

    My first pokemon

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,23 @@
 import "./intro.css";
 
 function App() {
-    return (
-        <div className="intro">
-            <div>Bienvenue sur ton futur pokédex !</div>
-            <div>
-                Tu vas pouvoir apprendre tout ce qu'il faut sur React, et
-                attraper des pokemons !
-            </div>
-            <div>Commence par créer ton premier pokemon: Mew !</div>
+  return (
+    <>
+      <div className="intro">
+        <div>Bienvenue sur ton futur pokédex !</div>
+        <div>
+          Tu vas pouvoir apprendre tout ce qu'il faut sur React, et attraper des
+          pokemons !
         </div>
-    );
+        <div>Commence par créer ton premier pokemon: Mew !</div>
+      </div>
+      <div>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/151.png" alt="Mew"/>
+        <p className="descriptionImg">Name : Mew</p>
+        <p className="descriptionImg">Id : 151</p>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- scripts/learnJS/learn.ts
+++ scripts/learnJS/learn.ts
@@ -14,34 +14,79 @@ const data = require("./data");
 //    5 x 9 = 45
 //    5 x 10 = 50
 const showMultiplicationTable = (number: number) => {
-    let result = ``;
-    for (let i = 1; i <= 10; i++) {
-        result = `${result}${number} x ${i} = ${number * i}\n`;
-    }
-    return result;
+  let result = ``;
+  for (let i = 1; i <= 10; i++) {
+    result = `${result}${number} x ${i} = ${number * i}\n`;
+  }
+  return result;
 };
 
 // 2. Ecrire une fonction qui prend en entrée une chaîne de caractère et qui la retourne en snake_case.
 //    Exemple :
 //    toSnakeCase("Hello World")
 //    "hello_world"
-const toSnakeCase = (str: string) => {};
+const toSnakeCase = (str: string) => {
+  const result = str.toLowerCase().replaceAll(" ", "_");
+  return result;
+};
 
-// 3. Ecrire une fonction qui prend en entrée un tableau de nombres et qui retourne un tableau avec la somme des entrées pairs
+// 3. Ecrire une fonction qui prend en entré
+// e un tableau de nombres et qui retourne un tableau avec la somme des entrées pairs
 //    et la somme des entrées impairs en deuxième.
 //    Exemple :
 //    sumPairsAndOdds([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 //    [16, 15]
-const sumPairsAndOdds = (numbers: number[]) => {};
+const sumPairsAndOdds = (numbers: number[]) => {
+  let sommeP = 0;
+  let sommeIP = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      sommeP = sommeP + numbers[i];
+    } else {
+      sommeIP = sommeIP + numbers[i];
+    }
+    //console.log(i%2);
+  }
+
+  console.log(sommeP, sommeIP);
+  return [sommeP, sommeIP];
+};
 
 // 4. Ecrire une fonction qui combine deux objets.
 //    Exemple :
 //    combine({a: 1, b: 8, d: 12}, {b: 10 c: 6, d: 7})
 //    {a: 1, b: 18, c: 6, d: 19}
 const combine = (
-    objA: { [key: string]: number },
-    objB: { [key: string]: number },
-) => {};
+  objA: { [key: string]: number },
+  objB: { [key: string]: number }
+) => {
+  // Boucle sur le plus grand objet
+  // let myVal = 0;
+
+  // for (let i = 0; i <= objA.length; i++) {
+  //   //name =
+  //   console.log(objA);
+  // }
+
+  //objA.forEach((keys(objA))=>console.log(Object.keys(objA)))
+  //console.log(Object.keys(objA), "OBB:" + Object.values(objB));
+  //mergeObj.forEach(Object.)
+  // console.log(mergeObj);
+
+  // Object.entries(objA).forEach(([key]) => {
+  //   if (key in objB) objA.key = objA[key] + objB[key];
+  //   //console.log(`${key}`, objA[key]);
+  // });
+
+  // return objA;
+  const result = { ...objA };
+
+  for (const [key, value] of Object.entries(objB)) {
+    result[key] = (result[key] || 0) + value;
+  }
+  return result;
+};
 
 // 5. Ecrire une fonction qui vérifie si un mot de passe est valide.
 //      Entre 8 - 20 caractères
@@ -54,35 +99,41 @@ const combine = (
 //    Exemple :
 //    isStrongPassword("Password123!")
 //    "Valide"
-const isStrongPassword = (password: string) => {};
+const isStrongPassword = (password: string) => {
+  const result = "Valide";
+
+  // if password.length > 7 and < 21
+  return result;
+};
 
 module.exports = {
-    showMultiplicationTable: {
-        fn: showMultiplicationTable,
-        inputs: data.showMultiplicationTableInputs,
-        outputs: data.showMultiplicationTableOutputs,
-        options: {
-            trim: true,
-        },
+  showMultiplicationTable: {
+    fn: showMultiplicationTable,
+    inputs: data.showMultiplicationTableInputs,
+    outputs: data.showMultiplicationTableOutputs,
+    options: {
+      trim: true,
     },
-    toSnakeCase: {
-        fn: toSnakeCase,
-        inputs: data.toSnakeCaseInputs,
-        outputs: data.toSnakeCaseOutputs,
-    },
-    sumPairsAndOdds: {
-        fn: sumPairsAndOdds,
-        inputs: data.sumPairsAndOddsInputs,
-        outputs: data.sumPairsAndOddsOutputs,
-    },
-    combine: {
-        fn: combine,
-        inputs: data.combineInputs,
-        outputs: data.combineOutputs,
-    },
-    isStrongPassword: {
-        fn: isStrongPassword,
-        inputs: data.isStrongPasswordInputs,
-        outputs: data.isStrongPasswordOutputs,
-    },
+  },
+  toSnakeCase: {
+    fn: toSnakeCase,
+    inputs: data.toSnakeCaseInputs,
+    outputs: data.toSnakeCaseOutputs,
+  },
+  sumPairsAndOdds: {
+    fn: sumPairsAndOdds,
+    inputs: data.sumPairsAndOddsInputs,
+    outputs: data.sumPairsAndOddsOutputs,
+  },
+  combine: {
+    fn: combine,
+    inputs: data.combineInputs,
+    outputs: data.combineOutputs,
+    options: { shouldSpread: true },
+  },
+  isStrongPassword: {
+    fn: isStrongPassword,
+    inputs: data.isStrongPasswordInputs,
+    outputs: data.isStrongPasswordOutputs,
+  },
 };
