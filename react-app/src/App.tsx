936950e74d14939333c8fbac0ba0b0440f871dd7
--get-all <valentin.chion@ext.theodo.com>
2025-02-24 12:17:31 +0100

    Correct code based on review

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,77 +4,84 @@ import "./intro.css";
 import Loader from "./components/Loader/index.tsx";
 
 interface Pokemon {
-  name: String;
-  id: number;
+    name: String;
+    id: number;
 }
 
 export default function App() {
-  const [isLoading, setIsLoading] = React.useState(true);
-  const [filterValue, setFilterValue] = React.useState("");
-  const [pokemons, setPokemons] = React.useState<Pokemon[]>([]);
-  const [filteredPokemons, setFilteredPokemons] = React.useState<Pokemon[]>([]);
-
-  const filterPokemonByName = (name: String) => {
-    const result = pokemons.filter((poke) =>
-      poke.name.toLowerCase().includes(name.toLowerCase())
+    const [isLoading, setIsLoading] = React.useState(true);
+    const [filterValue, setFilterValue] = React.useState("");
+    const [pokemons, setPokemons] = React.useState<Pokemon[]>([]);
+    const [filteredPokemons, setFilteredPokemons] = React.useState<Pokemon[]>(
+        [],
     );
 
-    return result;
-  };
+    const filterPokemonByName = (name: String) => {
+        const result = pokemons.filter((poke) =>
+            poke.name.toLowerCase().includes(name.toLowerCase()),
+        );
 
-  useEffect(() => {
-    setFilteredPokemons(filterPokemonByName(filterValue));
-  }, [filterValue]);
+        return result;
+    };
 
-  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) =>
-    setFilterValue(event.target.value);
+    useEffect(() => {
+        setFilteredPokemons(filterPokemonByName(filterValue));
+    }, [filterValue]);
 
-  const fetchPokemons = async () => {
-    const pokemonResponse = await fetch("https://pokeapi.co/api/v2/pokedex/2", {
-      headers: { accept: "application/json" },
-    });
-    const pokeObjects = await pokemonResponse.json();
-    const listPokes = pokeObjects.pokemon_entries;
-    const pokedex = listPokes.map(
-      (obj: { entry_number: number; pokemon_species: { name: string } }) => {
-        const id = obj.entry_number;
-        const name = obj.pokemon_species.name;
-        return { name, id };
-      }
+    const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+        setFilterValue(event.target.value);
+
+    const fetchPokemons = async () => {
+        const pokemonResponse = await fetch(
+            "https://pokeapi.co/api/v2/pokedex/2",
+            {
+                headers: { accept: "application/json" },
+            },
+        );
+        const pokeObjects = await pokemonResponse.json();
+        const listPokes = pokeObjects.pokemon_entries;
+        const pokedex = listPokes.map(
+            (obj: {
+                entry_number: number;
+                pokemon_species: { name: string };
+            }) => {
+                const id = obj.entry_number;
+                const name = obj.pokemon_species.name;
+                return { name, id };
+            },
+        );
+
+        setPokemons(pokedex);
+        setFilteredPokemons(pokedex);
+        setIsLoading(!isLoading);
+    };
+
+    useEffect(() => {
+        fetchPokemons();
+    }, []);
+
+    return (
+        <>
+            <Loader isVisible={isLoading} />
+            {!isLoading && (
+                <div>
+                    <div className="intro">
+                        <div>Bienvenue sur ton futur pokédex !</div>
+                        <div>
+                            Tu vas pouvoir apprendre tout ce qu'il faut sur
+                            React, et attraper des pokemons !
+                        </div>
+                        <div>Commence par créer ton premier pokemon: Mew !</div>
+                    </div>
+                    <div>
+                        <p>Cherchez votre pokémon : </p>
+                        <input onChange={onInputChange} value={filterValue} />
+                    </div>
+                    <div>
+                        <List contents={filteredPokemons} />
+                    </div>
+                </div>
+            )}
+        </>
     );
-
-    setPokemons(pokedex);
-    setFilteredPokemons(pokedex);
-    await 5000
-    setIsLoading(!isLoading);
-  };
-
-  useEffect(() => {
-    fetchPokemons();
-  }, []);
-
-  const loadDisplay = isLoading ? "none" : "block";
-
-  return (
-    <>
-      <Loader isVisible = {isLoading}/>
-      <div style={{ display: loadDisplay }}>
-        <div className="intro">
-          <div>Bienvenue sur ton futur pokédex !</div>
-          <div>
-            Tu vas pouvoir apprendre tout ce qu'il faut sur React, et attraper
-            des pokemons !
-          </div>
-          <div>Commence par créer ton premier pokemon: Mew !</div>
-        </div>
-        <div>
-          <p>Cherchez votre pokémon : </p>
-          <input onChange={onInputChange} value={filterValue} />
-        </div>
-        <div>
-          <List contents={filteredPokemons} />
-        </div>
-      </div>
-    </>
-  );
 }
--- react-app/src/components/Loader/index.tsx
+++ react-app/src/components/Loader/index.tsx
@@ -1,13 +1,22 @@
 import LoaderLogo from "./loader-logo.gif";
 
-export default function Loader({ isVisible }) {
-  const loadDisplay = isVisible ? "block" : "none";
-//   console.log(isVisible);
-  return (
-    <>
-      <div style={{ display: loadDisplay }}>
-        <img src={LoaderLogo} alt="loader" style={{display:"block",width:"auto", height:"500px" ,margin:"auto"}} />
-      </div>
-    </>
-  );
+export default function Loader({ isVisible }: { isVisible: boolean }) {
+    if (isVisible) {
+        return (
+            <div>
+                <img
+                    src={LoaderLogo}
+                    alt="loader"
+                    style={{
+                        display: "block",
+                        width: "auto",
+                        height: "500px",
+                        margin: "auto",
+                    }}
+                />
+            </div>
+        );
+    }
+
+    return null;
 }
