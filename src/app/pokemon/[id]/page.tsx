93b4f67fe3934eb6814ad01f2d00bbd868a22b7d
--get-all <valentin.chion@ext.theodo.com>
2025-02-27 17:26:13 +0100

    Correct things based on review

--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -1,32 +1,35 @@
 // "use client";
 import fetchPokeDetail from "@/core/services/loadDetail";
 import Image from "next/image";
+import "./page.css";
 
-export default async function Pokemon({
-  params,
+export default async function PokemonPage({
+    params,
 }: {
-  params: Promise<{ id: string }>;
+    params: Promise<{ id: string }>;
 }) {
-  // const { pokedex, numPokemon } = await fetchPoke();
-  const id = (await params).id;
-  const { name, imgTL, imgTR, imgBL, imgBR, typePokemon } =
-    await fetchPokeDetail({ id });
+    const id = (await params).id;
 
-  return (
-    <div>
-      <div>
-        <h1>{name}</h1>
-      </div>
-      <div>
-        <Image src={imgTL} width={200} height={200} alt={name} />
-        <Image src={imgTR} width={200} height={200} alt={name} />
-        <Image src={imgBL} width={200} height={200} alt={name} />
-        <Image src={imgBR} width={200} height={200} alt={name} />
-      </div>
-      <div>
-        <h1>Types :</h1>
-        <p>{typePokemon}</p>
-      </div>
-    </div>
-  );
+    const { name, imgTL, imgTR, imgBL, imgBR, typePokemon } =
+        await fetchPokeDetail({ id });
+
+    return (
+        <div className="pokemonContainer">
+            <div>
+                <h1>{name}</h1>
+            </div>
+            <div className="pokemonImages">
+                <Image src={imgTL} width={100} height={100} alt={name} />
+                <Image src={imgTR} width={100} height={100} alt={name} />
+                <Image src={imgBL} width={100} height={100} alt={name} />
+                <Image src={imgBR} width={100} height={100} alt={name} />
+            </div>
+            <div className="pokemonTypes">
+                <h1>Types :</h1>
+                {typePokemon.map((type: string) => (
+                    <div key={type}>{type}</div>
+                ))}
+            </div>
+        </div>
+    );
 }
